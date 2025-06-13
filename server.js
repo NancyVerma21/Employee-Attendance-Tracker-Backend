e89eedc40f670a93821f208ab8a8509165d7bfc2
NancyVerma21 <nancyverma002003@gmail.com>
2025-06-13 16:26:06 +0530

    adding updated vercel json

--- server.js
+++ server.js
@@ -31,3 +31,8 @@ mongoose.connect(process.env.MONGO_URI, {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 console.log('Connecting to MongoDB...');
+
+app.get('/', (req, res) => {
+  res.send('Employee Attendance Tracker API is running!');
+});
+
